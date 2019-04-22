ae31f0ba305b6c498af2889dc5a6a58c301d1ad7
Chanhaeng Lee <lch32111@naver.com>
2019-04-22 22:53:16 +0900

    upload vulkans

---
+++ VulkanPrac/main.cpp
@@ -0,0 +1,87 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <cstdlib>
+
+class HelloTriangleApplication 
+{
+public:
+	void run() 
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+private:
+	GLFWwindow* window;
+	const int WIDTH = 800;
+	const int HEIGHT = 600;
+
+	VkInstance instance;
+
+	void initWindow()
+	{
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	void initVulkan() 
+	{
+		createInstance();
+	}
+
+	void mainLoop() 
+	{
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+
+		}
+
+	}
+
+	void cleanup() 
+	{
+
+
+	}
+
+	void createInstance()
+	{
+		VkApplicationInfo appInfo = {};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+
+	}
+};
+
+int main() 
+{
+	std::cout << "r u running?";
+
+	HelloTriangleApplication app;
+
+	try 
+	{
+		app.run();
+	}
+	catch (const std::exception& e) 
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
